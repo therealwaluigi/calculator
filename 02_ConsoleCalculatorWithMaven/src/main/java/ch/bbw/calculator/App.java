491cbdbdf23c1cc21e8e20fc689a67b1f63d3c89
Nicolas Haemmerli <nicolas.haemmerli@gmx.ch>
2018-12-04 11:49:33 +0100

    implement into app class

--- 02_ConsoleCalculatorWithMaven/src/main/java/ch/bbw/calculator/App.java
+++ 02_ConsoleCalculatorWithMaven/src/main/java/ch/bbw/calculator/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Calculator calc = new Calculator();
+        
+        System.out.println("===================");
+        System.out.println("Summe: " + calc.summe(1, 2));
+
     }
 }
