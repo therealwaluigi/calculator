0002a3ff90fcab83b63877298b8829217ea92227
Nicolas Haemmerli <nicolas.haemmerli@gmx.ch>
2018-12-11 08:57:25 +0100

    initialize calculator in before method

--- 02_ConsoleCalculatorWithMaven/src/test/java/ch/bbw/calculator/CalculatorTest.java
+++ 02_ConsoleCalculatorWithMaven/src/test/java/ch/bbw/calculator/CalculatorTest.java
@@ -7,15 +7,18 @@ import org.junit.Test;
 public class CalculatorTest {
 	Calculator testee;
 	
+	@Before
+	public void init(){
+		testee = new Calculator();
+	}
+	
 	@Test
 	public void testSummeZweiPositive() {
-		testee = new Calculator();
 		assertTrue(testee.summe(2, 5) == 7);
 	}
 	
 	@Test
 	public void testSubtractionZweiPositive(){
-		testee = new Calculator();
 		assertTrue(testee.subtraction(5, 2) == 3);
 	}
 
