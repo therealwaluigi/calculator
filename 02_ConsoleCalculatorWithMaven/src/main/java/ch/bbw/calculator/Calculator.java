39a24116d9c6390d388ca25733075b44e20d63b0
Nicolas Haemmerli <nicolas.haemmerli@unic.com>
2018-12-21 08:11:08 +0100

    Add testcases for addition

--- 02_ConsoleCalculatorWithMaven/src/main/java/ch/bbw/calculator/Calculator.java
+++ 02_ConsoleCalculatorWithMaven/src/main/java/ch/bbw/calculator/Calculator.java
@@ -10,10 +10,17 @@ public class Calculator {
 		return zahl1-zahl2;
 	}
 	
-	public int division(int zahl1, int zahl2){
+	protected int division(int zahl1, int zahl2){
 		if(zahl2 == 0){
 			throw new IllegalArgumentException("Zweite Zahl darf nicht 0 sein");
 		}
 		return zahl1/zahl2;
 	}
+
+
+
+	/**
+	 * Private Methoden testet man nicht direkt.
+	 * Man testet die Methode, in der die private Methode aufgerufen wird.
+	 */
 }
--- 02_ConsoleCalculatorWithMaven/src/test/java/ch/bbw/calculator/CalculatorTest.java
+++ 02_ConsoleCalculatorWithMaven/src/test/java/ch/bbw/calculator/CalculatorTest.java
@@ -12,22 +12,61 @@ public class CalculatorTest {
 	public void init(){
 		testee = new Calculator();
 	}
-	
+
+	// Tests für Summe
 	@Test
 	public void testSummeZweiPositive() {
 		assertTrue(testee.summe(2, 5) == 7);
 	}
-	
+
+	@Test
+	public void testSummeZweiNegative(){
+		assertTrue(testee.summe(-5, -5) == -10);
+	}
+
+	@Test
+	public void testSummeNegativPositiv(){
+		assertTrue(testee.summe(-5, 10) == 5);
+	}
+
+	@Test
+	public void testSummeNullPositiv(){
+		assertTrue(testee.summe(0, 10) == 10);
+	}
+
+	@Test
+	public void testSummeNullNegativ(){
+		assertTrue(testee.summe(0, -10) == -10);
+	}
+
+	@Test
+	public void testSummeMaxValuePositiv(){
+		assertTrue(testee.summe(Integer.MAX_VALUE, 1) == Integer.MIN_VALUE);
+	}
+
+	@Test
+	public void testSummeMinValueNegativ(){
+		assertTrue(testee.summe(Integer.MIN_VALUE, -1) == Integer.MAX_VALUE);
+	}
+
+	@Test
+	public void testSummeErgebnisNull(){
+		assertTrue(testee.summe(0, 0) == 0);
+	}
+
+
+	// Tests für Subtraktion
 	@Test
 	public void testSubtractionZweiPositive(){
 		assertTrue(testee.subtraction(5, 2) == 3);
 	}
-	
+
+	// Tests für Division
 	@Test(expected=IllegalArgumentException.class)
 	public void testDivisionByZero(){
 		testee.division(5, 0);
 	}
-	
+
 	@Test
 	public void testDivisionByThree() throws IllegalArgumentException{
 			testee.division(5, 2);
