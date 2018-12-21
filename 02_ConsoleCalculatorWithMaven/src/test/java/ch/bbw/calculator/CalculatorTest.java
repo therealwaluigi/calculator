1934d5bcb0dc3e3554be0987a72bbba249f3eb6c
Nicolas Haemmerli <nicolas.haemmerli@unic.com>
2018-12-21 08:17:03 +0100

    add tests for subtraction

--- 02_ConsoleCalculatorWithMaven/src/test/java/ch/bbw/calculator/CalculatorTest.java
+++ 02_ConsoleCalculatorWithMaven/src/test/java/ch/bbw/calculator/CalculatorTest.java
@@ -61,6 +61,41 @@ public class CalculatorTest {
 		assertTrue(testee.subtraction(5, 2) == 3);
 	}
 
+	@Test
+	public void testSubtractionZweiNegative(){
+		assertTrue(testee.subtraction(-5, -5) == 0);
+	}
+
+	@Test
+	public void testSubtractionPositivNegativ(){
+		assertTrue(testee.subtraction(-5, 5) == -10);
+	}
+
+	@Test
+	public void testSubtractionPositivNull(){
+		assertTrue(testee.subtraction(5,0) == 5);
+	}
+
+	@Test
+	public void testSubtractionNegativNull(){
+		assertTrue(testee.subtraction(-5,0) == -5);
+	}
+
+	@Test
+	public void testSubtractionMaxValuePositiv(){
+		assertTrue(testee.subtraction(Integer.MAX_VALUE, 1) == Integer.MAX_VALUE-1);
+	}
+
+	@Test
+	public void testSubtractionMaxValueNegativ(){
+		assertTrue(testee.subtraction(Integer.MAX_VALUE, -1) == Integer.MIN_VALUE);
+	}
+
+	@Test
+	public void testSubtractionMinValuePositiv(){
+		assertTrue(testee.subtraction(Integer.MIN_VALUE, 1) == Integer.MAX_VALUE);
+	}
+
 	// Tests für Division
 	@Test(expected=IllegalArgumentException.class)
 	public void testDivisionByZero(){
